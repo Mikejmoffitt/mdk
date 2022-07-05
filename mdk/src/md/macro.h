23184588740285c3c2211204369d2df4dd5accb3
Michael Moffitt <mikejmoffitt@gmail.com>
2022-07-05 14:07:02 +0900

    Addition to macro

--- mdk/src/md/macro.h
+++ mdk/src/md/macro.h
@@ -1,6 +1,22 @@
 #ifndef MD_MACRO_H
 #define MD_MACRO_H
 
-#define ARRAYSIZE(x) ((sizeof(x) / sizeof(x[0])))
+#ifndef ARRAYSIZE
+#define ARRAYSIZE(x) (sizeof(x) / sizeof(x[0]))
+#endif  // ARRAYSIZE
+
+#ifndef MAX
+#define MAX(a, b) \
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+       _a > _b ? _a : _b; })
+#endif  // MAX
+
+#ifndef MIN
+#define MIN(a, b) \
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+       _a < _b ? _a : _b; })
+#endif  // MIN
 
 #endif  // MD_MARCO_H
