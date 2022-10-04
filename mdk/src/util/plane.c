d4b8d76accefcb6af40b728d2e9a0cb8a9d328d0
Michael Moffitt <mikejmoffitt@gmail.com>
2022-10-04 13:49:47 +0900

    plane mistake

--- mdk/src/util/plane.c
+++ mdk/src/util/plane.c
@@ -1,6 +1,6 @@
 #include "util/plane.h"
 
-static inline void plane_clear(VdpPlane plane)
+void plane_clear(VdpPlane plane)
 {
 	uint16_t plane_dest = md_vdp_get_plane_base(plane);
 	uint16_t plane_size = md_vdp_get_reg(VDP_PLANESIZE);
